5abedbd297bb5ceaa07a6bd4673ef34105980392
Euan740 <83963268+Euan740@users.noreply.github.com>
2021-05-21 12:58:35 -0500

    Update success.php

--- codeingniter/app/Views/success.php
+++ codeingniter/app/Views/success.php
@@ -6,7 +6,7 @@
     <div class="row text-center">
         <div class="col-sm-6 col-sm-offset-3">
         <br><br> <h1 style="color:#0fad00">{didier}</h1>
-        <br><br> <h2 style="color:#0fad00">{2332307a2F39baac3F58abc8fe436d32}</h2>
+        <br><br> <h2 style="color:#0fad00">{2332307a2f39baac3f58abc8fe436d32}</h2>
         </div>
     </div>
 </body>
